0f4143ab86892e05c431bf7cd79c35c1543d11d0
Vlad <vladanalyst@gmail.com>
2024-07-14 06:37:03 +0300

    Задание 10.4. выполнено

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref } from "vue";
-import { v4 as uuidv4 } from 'uuid';
+import { ref, onMounted } from "vue";
+import { collection, onSnapshot, addDoc, doc, deleteDoc, updateDoc } from "firebase/firestore";
+import { db } from '@/firebase';
 const todos = ref([
 //  { 
 //     id:'id1',
@@ -13,28 +14,46 @@ const todos = ref([
 //     done: false
 //   },
 ]);
+const todosCollectionRef = collection(db, "todo");
+
+onMounted(() => {
+onSnapshot(todosCollectionRef, (querySnapshot) => {
+  const fbTodos = [];
+  querySnapshot.forEach((doc) => {
+    const todo={
+    id: doc.id,
+    content: doc.data().content,
+    done:doc.data().done,
+  };
+  fbTodos.push(todo);
+  });
+  todos.value = fbTodos;
+});
+});
 
 const newtodoContent = ref("");
 
 const addToDo=()=>{
-  const newToDo = {
-    id: uuidv4(),
-    content:newtodoContent.value,
-    done: false
-  };
-  // console.log("Все работает", newToDo);
-  todos.value.unshift(newToDo);
+  addDoc(todosCollectionRef, {
+  content: newtodoContent.value,
+  done: false,
+});
   newtodoContent.value = "";
 };
 
 const deleteToDo = (id) => {
-    todos.value = todos.value.filter((todo) => todo.id !== id);
+   deleteDoc(doc(todosCollectionRef, id));
   };
 
 const toggleDone = (id) => {
   const index = todos.value.findIndex((todo) => todo.id === id);
-  todos.value[index].done = !todos.value[index].done;
+
+ updateDoc(doc(todosCollectionRef, id),{
+  done: !todos.value[index].done,
+});
+
 };
+
 </script>
 
 <template>
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,14 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbH4cmjIkNfGimveDXmtyH9qymqmQJDQs",
-  authDomain: "todofirst-aa38c.firebaseapp.com",
-  projectId: "todofirst-aa38c",
-  storageBucket: "todofirst-aa38c.appspot.com",
-  messagingSenderId: "38009622827",
-  appId: "1:38009622827:web:eb07462a7d531d9ae4756e",
-  measurementId: "G-KKVJPRYLW6"
-};
+    apiKey: "AIzaSyAQ-8-zWj1JBsvIgeQG0ghZz2EKLiAHVLw",
+    authDomain: "mytodo-6a418.firebaseapp.com",
+    projectId: "mytodo-6a418",
+    storageBucket: "mytodo-6a418.appspot.com",
+    messagingSenderId: "1012704569314",
+    appId: "1:1012704569314:web:1db49dba6474be5451bbbb"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
